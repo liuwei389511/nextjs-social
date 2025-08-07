6dbd92efb541ab875c881e0c1a134da00570bbef
liujian <liujian@zaxh.cn>
2025-08-07 18:24:39 +0800

    feat: first init

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+
+const nextConfig = {
+  experimental: {
+    reactCompiler: true,
+    // ppr: "incremental",
+  },
+};
+
+export default nextConfig;
---
+++ src/app/page.tsx
@@ -0,0 +1,6 @@
+/** @format */
+import Image from "next/image";
+
+export default function Page() {
+  return <Image src="/gift.png" alt="Profile" width={100} height={100} />;
+}
