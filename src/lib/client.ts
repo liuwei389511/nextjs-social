cbc9640213af066a5c68487aced14a023bf35dee
liujian <liujian@zaxh.cn>
2025-08-19 19:18:11 +0800

    feat: 添加数据库

---
+++ src/lib/client.ts
@@ -0,0 +1,10 @@
+import { PrismaClient } from "@/generated/prisma";
+const globalForPrisma = (globalThis as unknown) as {
+  prisma: PrismaClient | undefined;
+};
+
+const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
